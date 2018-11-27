2d454b9b01df4c8aa2bccb029f278893fb8a1b91
irsLu <keoneo@163.com>
2018-11-27 17:17:45 +0800

    add

--- Lintcode/Trailing Zeros/Solution.cpp
+++ Lintcode/Trailing Zeros/Solution.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 #include <string>
-
+#include <cmath>
 using namespace std;
 
-long long trailingZeros(long long n) {
-    // write your code here, try to do it without arithmetic operators.
-    long long res = 0;
-    while(n > 0)
-    {
-        res += n / 5;
-        n = n / 5;
-    }
-    
-    return res;
+ int digitCounts(int k, int n) 
+{ 
+	int count = 0; 
+	int num = n == 0 ? 0:  log10(n);
+	for (int i = 0; i <= num; i++) 
+	{ 
+		int currentWei = ((int)(n / pow(10,i))%10);//当前位 
+		
+		int leftWei = (int) (n/pow(10,i+1));
+		//高位 
+		int rightWei = (int) (n%(int)pow(10,i));
+		//低位 
+		if(currentWei > k )
+		{ 
+			count += (leftWei+1)*pow(10,i); 
+		}
+		else if(currentWei < k)
+		{ 
+			count += leftWei *pow(10,i); 
+		}else 
+		{ 
+			count += leftWei *pow(10,i)+rightWei+1; 
+		} 
+	} 
+	return count; 
 }
-   
 int main()
 {	
-	long long res = trailingZeros(105);
+	int res = digitCounts(0, 0);
 	cout<<res<<endl;
 	return 0;
 }
+
